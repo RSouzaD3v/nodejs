df84b8d6c784c4881f65fd41defd2489d2fa2d82
Rafael F. Souza <rafaelf.souzan02@gmail.com>
2024-10-04 02:22:59 -0300

    create user and getUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,44 @@ import User from "../models/User.js";
 export const userCreate = async (req, res) => {
     const { name, email, password } = req.body;
 
-    const user = new User({
-        name,
-        email,
-        password
-    });
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "Todos os campos precisam ser preenchidos" });
+    }
 
-    await user.save();
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "O email já está cadastrado" });
+        }
 
-    res.status(201).json({ message: "Usuário Criado com sucesso!", user });
-}
+        const user = new User({
+            name,
+            email,
+            password,  
+        });
+
+        await user.save(); 
+
+        res.status(201).json({ message: "Usuário criado com sucesso!", user });
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao criar usuário", details: error.message });
+    }
+};
+
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find().lean();
+
+        res.status(200).json({
+            error: false,
+            data: users
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: true,
+            message: 'Erro ao buscar usuários',
+            details: error.message,
+        });
+    }
+};
--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { userCreate } from './controllers/userController.js';
+import { getUsers, userCreate } from './controllers/userController.js';
 
 const router = express.Router();
 
+router.get('/users', getUsers);
 router.post('/users', userCreate);
 
 export { router };
