529807ef75a8e91ef437824c2e9138d639478699
Rafael F. Souza <rafaelf.souzan02@gmail.com>
2024-10-04 04:47:36 -0300

    CRUD USER

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,7 +35,6 @@ export const userCreate = async (req, res) => {
     }
 };
 
-
 export const getUsers = async (req, res) => {
     try {
         const users = await User.find().lean();
@@ -50,5 +49,49 @@ export const getUsers = async (req, res) => {
             message: 'Erro ao buscar usuários',
             details: error.message,
         });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    const { email } = req.body;
+
+    try{
+        const user = await User.findOne({ email }).lean();
+    
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" });
+        }
+    
+        await User.deleteOne({ email });
+    
+        res.status(200).json({ message: `Usuário com email ${email} deletado com sucesso` });
+    } catch(e) {
+        res.status(500).json({ message: "Erro ao deletar o usuário", details: error.message });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    const { name, email, password } = req.body; // Obtemos os dados do usuário a partir do corpo da requisição
+
+    try {
+        // Verificar se o usuário existe
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Atualizar as informações do usuário
+        await User.updateOne(
+            { email }, // Condição de busca
+            { $set: { name, password: hashedPassword } } // Dados a serem atualizados
+        );
+
+        // Retornar resposta de sucesso
+        res.status(200).json({ message: "Usuário atualizado com sucesso", user: { name, email, hashedPassword } });
+    } catch (error) {
+        // Retornar erro caso algo dê errado
+        res.status(500).json({ message: "Erro ao atualizar o usuário", details: error.message });
     }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 // routes/userRoutes.js
 import express from 'express';
-import { userCreate, getUsers } from '../controllers/userController.js';
+import { userCreate, getUsers, deleteUser, updateUser } from '../controllers/userController.js';
 import { login } from '../controllers/authController.js';
 import { authMiddleware } from '../middleware/auth.js';
 
@@ -14,5 +14,8 @@ router.post('/register', userCreate);
 
 // Rota protegida: obter lista de usuários (somente para usuários autenticados)
 router.get('/users', authMiddleware, getUsers);
+router.delete('/users', authMiddleware, deleteUser);
+
+router.put('/users', authMiddleware, updateUser);
 
 export default router;
